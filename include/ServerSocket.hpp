daa5026aaa7b2632403f4a5d8338d30b6b35ee06
88-yana <85784752+88-yana@users.noreply.github.com>
2023-05-05 17:29:54 +0900

    ADD:

---
+++ include/ServerSocket.hpp
@@ -0,0 +1,43 @@
+#ifndef SERVERSOCKET_HPP
+# define SERVERSOCKET_HPP
+
+
+# include <iostream>
+# include <string>
+# include <unistd.h>
+// # include <ctype.h>
+// # include <errno.h>
+// # include <signal.h>
+// # include <stdlib.h>
+// # include <sysexits.h>
+
+# include <sys/param.h>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+# include <arpa/inet.h>
+# include <netinet/in.h>
+# include <netdb.h>
+
+using namespace std;
+
+class ServerSocket
+{
+	public:
+		// Constructors
+		ServerSocket(string port);
+		
+		// Destructor
+		~ServerSocket();
+		
+		// Member Functions
+		int makeSocket(string port);
+		int getFd() const;
+	private:
+		int _socketFd;
+		int result;
+		int makeAddressInfo(string port, struct addrinfo **res);
+};
+
+#endif
